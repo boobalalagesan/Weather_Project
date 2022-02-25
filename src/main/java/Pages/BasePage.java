b6cfe2c46db2275dedcc10578938d59aea8033bd
Boobal Alagesan <boobalalagesan@gmail.com>
2022-02-25 10:37:42 +0530

    Updated code_25-2

--- src/main/java/Pages/BasePage.java
+++ src/main/java/Pages/BasePage.java
@@ -15,41 +15,42 @@ import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
 
 import Utils.RunConfig;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BasePage {
-public WebDriver driver;
-public ExtentTest test;
-public BasePage() {
-	
-}
-public BasePage(ExtentTest test, WebDriver driver) {
-	this.driver=driver;
-	this.test=test;
-}
-    public void takeScreenshot() {
-    	try {
-		String path=((TakesScreenshot)driver).getScreenshotAs(OutputType.BASE64);
-		
+	public WebDriver driver;
+	public ExtentTest test;
+	public BasePage() {
+
+	}
+	public BasePage(ExtentTest test, WebDriver driver) {
+		this.driver=driver;
+		this.test=test;
+	}
+	public void takeScreenshot() {
+		try {
+			String path=((TakesScreenshot)driver).getScreenshotAs(OutputType.BASE64);
+
 			test.log(Status.INFO, "ScreenShot",MediaEntityBuilder.createScreenCaptureFromBase64String(path).build());
 		} 
-    	catch (IOException e) {
-    		
+		catch (IOException e) {
+
 			e.printStackTrace();
 		}
 	}
-    
-    public WebDriver openBrowser(String Browser) {
-    	
-    	if(Browser.contentEquals("Chrome")) {
+
+	public WebDriver openBrowser(String Browser) {
+
+		if(Browser.contentEquals("Chrome")) {
 			WebDriverManager.chromedriver().setup();
-                        driver = new ChromeDriver();
+			driver = new ChromeDriver();
 		}
 		else if (Browser.contentEquals("Firefox")) {
 			System.setProperty("webdriver.gecko.driver",RunConfig.MOZILLA_DRIVER_EXE);
 			driver = new FirefoxDriver();
 		}
 		else if(Browser.contentEquals("Edge")) {
-		System.setProperty("webdriver.edge.driver", RunConfig.EDGE_DRIVER_EXE);
+			System.setProperty("webdriver.edge.driver", RunConfig.EDGE_DRIVER_EXE);
 		}
 		driver.manage().window().maximize();
 		driver.get(RunConfig.Test_ApplicationURL);
