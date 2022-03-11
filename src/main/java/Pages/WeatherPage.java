d0c081b415b329e277fc458fb7dab68392f2b894
Boobal Alagesan <boobalalagesan@gmail.com>
2022-03-11 09:58:48 +0530

    changed ProjectName

--- src/main/java/Pages/WeatherPage.java
+++ src/main/java/Pages/WeatherPage.java
@@ -15,70 +15,66 @@ import API.Weather;
 import Utils.RunConfig;
 import org.testng.Assert;
 
-public class WeatherPage extends BasePage{
-	
-	public WeatherPage(WebDriver driver,ExtentTest test) {
-		super(driver,test);
+public class WeatherPage extends BasePage {
+
+	public WeatherPage(WebDriver driver, ExtentTest test) {
+		super(driver, test);
 	}
-	
+
 	public void getUITempValue(String city) throws InterruptedException, IOException {
-		
-		
-		 List<WebElement> understandEle=driver.findElements(By.xpath("//div[@class='banner-button policy-accept']"));
-		if(understandEle.size()!=0) {
+
+		List<WebElement> understandEle = driver.findElements(By.xpath("//div[@class='banner-button policy-accept']"));
+		if (understandEle.size() != 0) {
 			for (WebElement webElement : understandEle) {
 				webElement.click();
 			}
 		}
-		WebElement searchBar=driver.findElement(By.xpath("//input[@placeholder='Search']"));
-		searchBar.sendKeys(city+Keys.ENTER);
+		WebElement searchBar = driver.findElement(By.xpath("//input[@placeholder='Search']"));
+		searchBar.sendKeys(city + Keys.ENTER);
 		TimeUnit.SECONDS.sleep(2);
-		WebElement Suggest=driver.findElement(By.xpath("//div[@class='content-module']//a[1]"));
+		WebElement Suggest = driver.findElement(By.xpath("//div[@class='content-module']//a[1]"));
 		Suggest.click();
 		TimeUnit.SECONDS.sleep(2);
-		List<WebElement> totalFrames = driver.findElements(By.id("google_ads_iframe_/6581/web/in/interstitial/admin/search_0"));
+		List<WebElement> totalFrames = driver
+				.findElements(By.id("google_ads_iframe_/6581/web/in/interstitial/admin/search_0"));
 
-		if(!(totalFrames.size()==0)) {
+		if (!(totalFrames.size() == 0)) {
 			driver.switchTo().frame("google_ads_iframe_/6581/web/in/interstitial/admin/search_0");
-			WebElement dismissButton=driver.findElement(By.xpath("//div[@id='dismiss-button']/div"));
+			WebElement dismissButton = driver.findElement(By.xpath("//div[@id='dismiss-button']/div"));
 			dismissButton.click();
 			driver.switchTo().defaultContent();
 		}
 
-		WebElement tempEle=driver.findElement(By.xpath("//div[@class='temp'][1]"));
-		
-		StringBuffer sb= new StringBuffer(tempEle.getText().toString());  
-		for(int i=1;i<=2;i++) {
-			sb.deleteCharAt(sb.length()-1); 
+		WebElement tempEle = driver.findElement(By.xpath("//div[@class='temp'][1]"));
+
+		StringBuffer sb = new StringBuffer(tempEle.getText().toString());
+		for (int i = 1; i <= 2; i++) {
+			sb.deleteCharAt(sb.length() - 1);
 		}
-		String Stringcount=sb.toString();
-		int actualcount=Integer.parseInt(Stringcount);
-		test.log(Status.PASS, "Temperature from UI is "+actualcount);
+		String Stringcount = sb.toString();
+		int actualcount = Integer.parseInt(Stringcount);
+		test.log(Status.PASS, "Temperature from UI is " + actualcount);
 		takeScreenshot();
 		getAPIValue(city, actualcount);
-
+		
 
 	}
 
 	public void getAPIValue(String city, int actualcount) throws IOException {
-		Weather weather=new Weather();
-		int expectedValue= weather.getTemp(city);
-		test.log(Status.PASS, "Temperature from API is "+expectedValue);
-		int lowVar=expectedValue-1;
-		int highvar=expectedValue+1;
-		
-		if((actualcount>=lowVar)&&(actualcount<=highvar)) {
+		Weather weather = new Weather();
+		int expectedValue = weather.getTemp(city);
+		test.log(Status.PASS, "Temperature from API is " + expectedValue);
+		int lowVar = expectedValue - 1;
+		int highvar = expectedValue + 1;
+
+		if ((actualcount >= lowVar) && (actualcount <= highvar)) {
 			test.log(Status.PASS, "Temperatures are equal");
-		}
-		else {
+		} else {
 			test.log(Status.FAIL, "Temperatures are not equal");
 			Assert.fail("Temperatures are not equal");
 
 		}
 
 	}
-	
-		
-	}
-	
 
+}
--- src/test/java/TestPack/CompareResult.java
+++ src/test/java/TestPack/CompareResult.java
@@ -1,4 +1,5 @@
 package TestPack;
+
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
@@ -25,51 +26,52 @@ import Pages.WeatherPage;
 import Utils.RunConfig;
 import email.SendMailAfterExec;
 import jxl.read.biff.BiffException;
+
 public class CompareResult extends BasePage {
 	WebDriver driver;
 	WeatherPage weatherPage;
-	
+
 	@Test(dataProvider = "cityData")
-	public void TempCompare(String RunMode,String Browser,String city) throws InterruptedException, IOException {
-		
-			test=extent.createTest("Comaparison value for "+city);
-			driver=openBrowser(Browser);
-			weatherPage=new WeatherPage(driver, test);
-			PageFactory.initElements(driver, weatherPage);
-			try {
-				weatherPage.getUITempValue(city);
-			} 
-			catch (InterruptedException e) {
-				e.printStackTrace();
-				Assert.fail("Exception occured "+e);
-			}
+	public void TempCompare(String RunMode, String Browser, String city) throws InterruptedException, IOException {
+
+		test = extent.createTest("Comaparison value for " + city);
+		driver = openBrowser(Browser);
+		weatherPage = new WeatherPage(driver, test);
+		PageFactory.initElements(driver, weatherPage);
+		try {
+			weatherPage.getUITempValue(city);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			Assert.fail("Exception occured " + e);
 		}
-	
+
+	}
 
 	@AfterTest
 	public void close() {
 		extent.flush();
-		driver.quit();
-
-		SendMailAfterExec sendMailAfterExec= new SendMailAfterExec();
+		driver.close();
+		SendMailAfterExec sendMailAfterExec = new SendMailAfterExec();
 		try {
-			//sendMailAfterExec.sendMail();
+			// sendMailAfterExec.sendMail();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 	@AfterSuite
-	public void openReport()  {
+	public void openReport() {
 		try {
-			Desktop.getDesktop().browse(new File(RunConfig.REPORT_PATH+".html").toURI());
+			Desktop.getDesktop().browse(new File(RunConfig.REPORT_PATH + ".html").toURI());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	@DataProvider(name="cityData")
+
+	@DataProvider(name = "cityData")
 	public String[][] cityData() throws BiffException, IOException {
-		ExcelData excelData=new ExcelData(RunConfig.DATA_PATH);
-		String[][] data=excelData.getData("RunSheet");
+		ExcelData excelData = new ExcelData(RunConfig.DATA_PATH);
+		String[][] data = excelData.getData("RunSheet");
 		return data;
 	}
 }
